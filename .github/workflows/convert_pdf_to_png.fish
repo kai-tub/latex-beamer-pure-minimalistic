cfe1eaf1f629e064f47ce0a4bd42124e4dd39f1c
Kai Norman Clasen <kainorman.clasen@novatec-gmbh.de>
2020-05-25 10:20:10 +0200

    Test if fish version is too old

--- .github/workflows/convert_pdf_to_png.fish
+++ .github/workflows/convert_pdf_to_png.fish
@@ -1,20 +1,24 @@
 #!/bin/fish
 
 function convert_minimal_examples
-    set files (find ./minimal_examples -type d -path '*/.git' -prune -o -name '*.pdf' -print0 | xargs -0)
-    for file_ in "$files"
+    set files (find ./minimal_examples -type d -path '*/.git' -prune -o -name '*.pdf' -print0 | string split0)
+    for file_ in $files
+        echo "$file_"
         set target (string replace '.pdf' '.png' "$file_")
         convert -density 500 "$file_"[0] "$target"
     end
 end
 
 function convert_compare_examples
-    set files (find ./compare_examples -type d -path '*/.git' -prune -o -name '*example*.pdf' -print0 | xargs -0)
-    for file_ in "$files"
-        set target (string replace '.pdf' '-%02d.png' "$file_")
-        set montage_target (string replace '.pdf' '.png' "$file_")
-        convert -density 500 "$file_" "$target"
-        montage *.png -geometry +2+2 -tile 2x -background none "$montage_target"
+    set files (find ./compare_examples -type d -path '*/.git' -prune -o -name '*example*.pdf' -print0 | string split0)
+    for file_ in $files
+        echo "$file_"
+        set convert_target (string replace -r '(.*)\.pdf' '$1-%02d.png' "$file_")
+        set convert_source (string replace -r '(.*)\.pdf' '$1-*.png' "$file_")
+        set montage_target (string replace -r '(.*)\.pdf' '$1.png' "$file_")
+        convert -density 500 "$file_" "$convert_target"
+        montage "$convert_source" -geometry +2+2 -tile x2 -background none "$montage_target"
+        # maybe next to each other
     end
 end
 
